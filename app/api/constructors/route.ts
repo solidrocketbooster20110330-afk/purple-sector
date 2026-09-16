82d5e1be804379188809def0debfbbc4a991bcb3
solidrocketbooster20110330-afk <solid.rocket.booster20110330@gmail.com>
2026-09-17 00:58:16 +0900

    Update route.ts

--- app/api/constructors/route.ts
+++ app/api/constructors/route.ts
@@ -1,1 +1,9 @@
-
+export async function GET() {
+  return Response.json([
+    { position: 1, team: "McLaren", points: 658 },
+    { position: 2, team: "Red Bull Racing", points: 602 },
+    { position: 3, team: "Mercedes", points: 519 },
+    { position: 4, team: "Ferrari", points: 487 },
+    { position: 5, team: "Aston Martin", points: 211 }
+  ]);
+}

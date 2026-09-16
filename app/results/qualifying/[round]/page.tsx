68fd9d1272e9b94d3deba9169f9bfdb178e9f414
solidrocketbooster20110330-afk <solid.rocket.booster20110330@gmail.com>
2026-09-17 03:45:48 +0900

    Update page.tsx

--- app/results/qualifying/[round]/page.tsx
+++ app/results/qualifying/[round]/page.tsx
@@ -1,5 +1,5 @@
-import BottomNav from "../../components/BottomNav";
-import ResultsTabs from "../ResultsTabs";
+import BottomNav from "../../../components/BottomNav";
+import ResultsTabs from "../../ResultsTabs";
 
 type QualifyingResult = {
   position: string;

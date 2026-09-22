ba2b0863aad9176f858d47b5bec5d369ec4567ce
solidrocketbooster20110330-afk <solid.rocket.booster20110330@gmail.com>
2026-09-23 00:28:53 +0900

    Update page.tsx

--- app/results/qualifying/[round]/page.tsx
+++ app/results/qualifying/[round]/page.tsx
@@ -1,4 +1,5 @@
 import BottomNav from "../../../components/BottomNav";
+import ResultsTabs from "../../ResultsTabs";
 
 type SessionResult = {
   position: number;
@@ -143,6 +144,8 @@ export default async function QualifyingPage({
       >
         {raceName}
       </p>
+
+      <ResultsTabs />
 
       <div
         style={{
